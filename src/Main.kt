20e202430a334d608bf66fc8f48ea13d3e343a8e
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-08 11:12:50 +0100

    added some types

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
-    val age = 28
+    // val age = 28
     // age = 99 //val is like a constant
+
+    // var age = 28
+    var age : Int = 28
+    // age = "fourteen"   //type cannot be changed from it's original type
+    // age = 22.4   //type cannot be changed from it's original type
     println(age)
-    var candy = "snickers"
-    println(candy)
+
+    var candy : String = "snickers"
+    // candy = 28 //type cannot be changed from it's original type
+
+    var sample : Double
+    sample = 12.5
+
+    println(sample)
 }
