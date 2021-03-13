c4f2c02b4d83b945169cef5027131f05eb43c01e
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-13 12:32:41 +0100

    array, list and mutableList

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+  var topCandies : List<String> = listOf("Snickers", "Lindt")
+  var badCandies : Array<String> = arrayOf("xxxxx one", "yyyy other")
+  var similarCandies : MutableList<String> = mutableListOf("Xnickers", "Nindt")
+
+  // topCandies[0] = "A new good one" List element cannot be changed
+  badCandies[0] = "The bad One"
+  println(badCandies.last())
+
+  similarCandies.add(1, "Xilka")
+
+  println(similarCandies[0])
+  println(similarCandies[1])
 }
