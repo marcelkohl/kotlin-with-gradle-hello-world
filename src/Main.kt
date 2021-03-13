80ac5f0ca72174c590d464291722e3b24b399f2a
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-13 12:41:52 +0100

    some loops and interacting in array

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+  for (count in 1..10) {
+    println(count)
+  }
+
+  for (count in 5..8) {
+    println(count)
+  }
+
+  var topCandies : MutableList<String> = mutableListOf("Snickers", "Milka", "Lindt")
+
+  for (candy in topCandies) {
+    println(candy)
+  }
 }
