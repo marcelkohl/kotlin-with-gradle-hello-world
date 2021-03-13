e54f0c8185e579648882e9f94db32f0bfbebe191
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-13 12:18:23 +0100

    some boolean operations with conditionals

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+  var age : Int = 40
+  var weight : Double = 56.4
+  var candy : String = "xSnickers"
+  var catIsAlive: Boolean = false
+
+  var canRideCar : Boolean = age >= 18
+  var isCandyGood : Boolean = candy == "Chocolate"
+
+  // print(canRideCar)
+  // print(isCandyGood)
+
+  if (catIsAlive) {
+    println("The cat lives!")
+  } else {
+    println("RIP the cat :(")
+  }
+
+  if (candy !== "Snickers") {
+    println("Candy must be a snickers :\\")
+  } else {
+    println("Yeah, we have snickers :D")
+  }
 }
