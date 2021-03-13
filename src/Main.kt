8cb9495133010ccdec4e5ffdfd231118402b715f
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-13 12:07:13 +0100

    some math

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+    var total = 0
+    total = total + 100
+    println(total)
+
+    total = total - 33
+    println(total)
+
+    total += 33
+    println(total)
+
+    total -= 10
+    println(total)
+
+    total++
+    println(total)
+
+    total--
+    println(total)
+
+    var pi : Double = 3.14
+    var radius : Int = 3
+    var circunference : Int = pi.toInt() * radius
+
+    println(circunference)
 }
