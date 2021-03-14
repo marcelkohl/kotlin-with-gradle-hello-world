34997b2427e9877f4f86d015b97346fc65a95af1
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-14 10:31:23 +0100

    creating a class

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+  class Dog(name: String, age: Int) {
+    var name : String = name
+    var age : Int = age
+  }
+
+  var dog = Dog("Junky", 10)
+
+  println(dog.name)
+
+  dog.name = "New Fido"
+
+  println(dog.name)
+  println(dog.age)
 }
