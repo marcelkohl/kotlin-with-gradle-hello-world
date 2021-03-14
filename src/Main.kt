1fe1ac72cae7c6d81432835f69dc1add078b4a5f
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-14 10:13:39 +0100

    some mapping

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+  var dogs = mapOf<String, Int>("Fido" to 8, "Sara" to 15, "Sean" to 54)
+
+  println(dogs["Fido"]);
+
+  for ((k, v) in dogs) {
+    println("The dog $k has $v years old");
+  }
 }
