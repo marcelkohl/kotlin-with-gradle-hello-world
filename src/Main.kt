0f75cb20068e9027bf4c91b8cdba95e6b3dae17d
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-14 10:54:43 +0100

    added some class methods

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
     println("Hello World")
+
+    class Dog(name:String, age:Int) {
+      var name: String = name
+      var age: Int = age
+
+      fun info() {
+        println("$name is an animal and has $age years old")
+      }
+
+      fun addAge(age: Int) {
+        this.age += age
+      }
+    }
+
+    var dog = Dog("Pretinha", 11)
+
+    dog.info()
+    dog.addAge(3)
+
+    println(dog.age)
 }
