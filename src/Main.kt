79a383e615717cab8f8cea338ee8008d9b2f195e
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-03-14 10:25:44 +0100

    some functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,36 @@
 fun main(args: Array<String>) {
+  fun hello() {
     println("Hello World")
+  }
+
+  hello()
+
+  fun addAndPrnt(number1: Int, number2: Int) {
+    println(number1 + number2)
+  }
+
+  addAndPrnt(2, 5)
+
+  fun addIfExistsAndPrint(number1: Int = 0, number2: Int = 0): Int {
+    var result : Int = number1 + number2
+
+    println(result)
+
+    return result
+  }
+
+  addIfExistsAndPrint(number2 = 8)
+
+  fun printOptionalIfExists(name: String, age: Int = 0) {
+    println("name is $name and age is $age")
+  }
+
+  printOptionalIfExists("Roger")
+
+  fun calculateForMe(number1: Int, number2: Int): Int {
+    return addIfExistsAndPrint(number1, number2)
+  }
+
+  println("calculation is " + calculateForMe(2, 3))
+
 }
